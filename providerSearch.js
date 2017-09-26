0d894557cea3e30e6db8cde74b8addf187fa0aa4
Robin Luo <RPHLuo@github.com>
2017-09-26 15:16:51 +0000

    implemented basic search provider

--- providerSearch.js
+++ providerSearch.js
@@ -3,70 +3,80 @@ const deepstream = require('deepstream.io-client-js');
 const provider = deepstream('tutor-back.blindside-dev.com:6020');
 
 provider.login({
-  username: 'server',
+  username: 'provider',
   password: 'sp'
+}, (success, data) => {
+  console.log(success);
 });
 var connection = null;
 r.connect( {host: 'localhost', port: 28015}, function(err, conn) {
   if (err) throw err;
   connection = conn;
-  r.db('deepstream').table('user').filter(function(tutor) { return tutor('categories').contains('English')})
-.run(connection, function(err, cursor) {
-    if (err) throw err;
-    cursor.toArray(function(err, result) {
-      if (err) throw err;
-      for (var a in result) {
-		console.log(result[a].username);
-      }
-      //console.log(JSON.stringify(result, null, 2));
-    });
-  });
-  
-  provider.event.listen('tutor/.*', function(subject, isSubscribed, response) {
-    if (isSubscribed) {
-      response.accept();
-      var category = subject.split('/')[1];
-      provider.event.emit();
-    }else {
-
-    }
-  });
-
-  provider.event.listen('search/.*', function(subject, isSubscribed, response) { 
-    if (isSubscribed) {
-      response.accept();
-      query(subjectfunction (tutors) { 
-        provider.event.emit(subject, {data: );
-      }
-    }else {
-  
-    }
-  });
-
 });
-/*
+
+provider.event.listen('tutor/.*', function(subject, isSubscribed, response) {
+  if (isSubscribed) {
+    subscribe(subject.split('/')[1], function (tutors, subject) {
+      provider.event.emit('tutor/'+subject, {subject: subject, data: tutors});
+    });
+  }else {
+    console.log('tutor listen ended');
+  }
+});
+
 provider.event.listen('search/.*', function(subject, isSubscribed, response) {
   if (isSubscribed) {
     response.accept();
-    
-    provider.event.emit();
+    query(subject.split('/')[1], function (tutors) {
+      provider.event.emit(subject, {subject: subject, data: tutors});
+    });
   }else {
-    
-  }
+ 
+ }
 });
-*/
+
+provider.rpc.provide('search/tutor', function (data, response ) {
+  var subject = data.subject;
+  query(subject, function(tutors) {
+    response.send({data:tutors, subject: subject});
+  });
+});
+
+function subscribe(category, callback) {
+//  r.db('deepstream').table('user').filter(function(tutor) { return tutor('categories').contains(category)})
+ r.db('deepstream').table('user').filter(function(tutor) { return tutor('subjects').contains(category)})
+
+  .changes()
+  .run(connection, function(err, cursor) {
+    cursor.each(() => {
+      r.db('deepstream').table('user').filter(function(tutor) { return tutor('subjects').contains(category)})
+      .run(connection, function(err, cursor) {
+        if(err) throw err;
+        cursor.toArray(function(err, result) {
+          if (err) throw err;
+          var tutors = [];
+          for (var i in result) {
+            tutors.push(result[i]);
+          }
+          callback(tutors, category);
+        });
+      });
+   });
+  });
+}
 
 function query(category, callback) {
-  r.db('deepstream').table('user').filter(function(tutor) { return tutor('categories').contains(category)})
+  r.db('deepstream').table('user').filter(function(tutor) { return tutor('subjects').contains(category)})
   .run(connection, function(err, cursor) {
     if (err) throw err;
     cursor.toArray(function(err, result) {
       if (err) throw err;
       var tutors = [];
       for (var i in result) {
-        tutors.push(result[i].username);
+        tutors.push(result[i]);
       }
       callback(tutors);
     });
   });
 }
+
