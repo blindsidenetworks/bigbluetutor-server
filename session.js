34c0a25dd25d49e1fd21ee513a348fd8b636e1fb
cody <cody.browne@blindsidenetworks.com>
2017-10-12 13:15:07 -0400

    Removed files that are no longer used

--- session.js
+++
@@ -1,7 +0,0 @@
-var express = require("express");
-var expressSession = require("express-session");
-var passport = require("passport");
-
-var session = expressSession({name: "Session", secret: "nyan", resave: true, saveUninitialized: true});
-
-module.exports.session = session;
