99cf7612f3852bfff0106ff1a6fa529b5e6b2334
cody <cody.browne@blindsidenetworks.com>
2017-10-27 15:44:29 -0400

    Python scripts now use .env variables

--- scripts/cleartables.py
+++ scripts/cleartables.py
@@ -1,16 +1,20 @@
 import rethinkdb as r
+import dotenv
+import os
+import sys
+
+dotenv.load_dotenv("./.env")
 
 #If True, the Tables will be dropped, deleting the tables and their contents.
 #If False, only the contents of the tables will be deleted. The tables themselves will remain.
-dropTables = False
 
-r.connect("localhost", 28015).repl()
+r.connect(os.environ.get("DB_HOST"), int(os.environ.get("DB_PORT"))).repl()
 list = r.db("deepstream").table_list().run()
 print("Table list:")
 print(list)
 print()
 
-if(dropTables):
+if(len(sys.argv) > 1 and sys.argv[1] == "drop"):
     for table in list:
         print(r.db("deepstream").table_drop(table).run())
 else:
