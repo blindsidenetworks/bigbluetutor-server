94f59720fa8284b1fc52e3bd8dbf24a71714fe00
Robin Luo <robinpluo@gmail.com>
2017-11-01 16:30:57 -0400

    Revert "updated client.js"

--- client.js
+++ client.js
@@ -59,14 +59,9 @@ deepstreamClient.rpc.provide('requestMeeting', (data, response) => {
           var messages = record.get('messages');
           var clientMessages = clientRecord.get('messages');
 
-          if(clientRecord.get('meeting') != '') {
-            return;
-          }
-
           if (clientPendingMeetings.indexOf(contact) != -1) {
 
             var i = -1;
-            var j = -1;
             messages[client].messages.some(function(item, index) {
               if(item.active) {
                 i = index;
@@ -75,39 +70,24 @@ deepstreamClient.rpc.provide('requestMeeting', (data, response) => {
               return false;
             });
 
+            if (i!=-1) {
+              messages[client].messages.splice(i,1,{user: client, message: "session in progress", special: "ActiveSession", active: false});
+              record.set('messages', messages);
+            }
+
+            i = -1;
             clientMessages[contact].messages.some(function(item, index) {
               if(item.active) {
-                j = index;
+                i = index;
                 return true;
               }
               return false;
             });
 
-            if (record.get('meeting') != '') {
-              if (i!=-1) {
-                messages[client].messages.splice(i,1,{user: contact, message: "Session Declined", special: "DeclinedRequest", active: false});
-                record.set('messages', messages);
-              }
-
-              if (j!=-1) {
-                clientMessages[contact].messages.splice(j,1,{user: contact, message: "Session Declined", special: "DeclinedRequest", active: false});
-                clientRecord.set('messages', clientMessages);
-              }
-              return;
-            } else {
-              record.set('meeting',true);
-              if (i!=-1) {
-                messages[client].messages.splice(i,1,{user: client, message: "session in progress", special: "ActiveSession", active: true});
-                record.set('messages', messages);
-              }
-
-              if (j!=-1) {
-                clientMessages[contact].messages.splice(j,1,{user: client, message: "session in progress", special: "ActiveSession", active: true});
-                clientRecord.set('messages', clientMessages);
-              }
-
+            if (i!=-1) {
+              clientMessages[contact].messages.splice(i,1,{user: client, message: "session in progress", special: "ActiveSession", active: false});
+              clientRecord.set('messages', clientMessages);
             }
-
 
             clientPendingMeetings.splice(clientPendingMeetings.indexOf(contact), 1);
             requestMeetings.splice(requestMeetings.indexOf(client), 1);
@@ -125,16 +105,19 @@ deepstreamClient.rpc.provide('requestMeeting', (data, response) => {
             userRecord.whenReady(() => {
             clientUserRecord.whenReady(() => {
             if (!messages[client]) {
-              messages[client] = {pic: clientUserRecord.get('profilePic'), messages:[]};
+              messages[client] = {pic: clientUserRecord.get('profilePic'),messages:[]};
             }
             if (!clientMessages[contact]) {
-              clientMessages[contact] = {pic: userRecord.get('profilePic'), messages:[]};
+              clientMessages[contact] = {pic: userRecord.get('profilePic'),messages:[]};
             }
 
-            if( data && data.categories) {
-              clientMessages[contact].messages.push({user: client, message: 'I would like to request a tutoring session for '+data.categories+'. The preferred tutoring session length is '+data.time+' minutes.', special: false});
-              messages[client].messages.push({user: client, message: 'I would like to request a tutoring session for '+data.categories+'. The preferred tutoring session length is '+data.time+' minutes.', special: false}); 
+            if (!messages[client]) {
+              messages[client] = [];
             }
+            if (!clientMessages[contact]) {
+              clientMessages[contact] = [];
+            }
+
             messages[client].messages.push({user: client, message: "Meeting Request" , special: "IncomingRequest", active: true, data: data});
             clientMessages[contact].messages.push({user: client, message: "Waiting for " + client, special: "OutgoingRequest", active: true, data: data});
             record.set('messages',messages);
@@ -146,64 +129,6 @@ deepstreamClient.rpc.provide('requestMeeting', (data, response) => {
             });
             });
           }
-        });
-      });
-    }
-  });
-});
-
-deepstreamClient.rpc.provide('endMeeting', (data, response) => {
-    var contact = data.contact;
-  var client = data.client;
-  var data = data.data;
-  if (client === contact) { return }
-  deepstreamClient.record.has("profile/"+contact, (err, has) => {
-    if (has) {
-      var record = deepstreamClient.record.getRecord("profile/"+contact);
-      var clientRecord = deepstreamClient.record.getRecord("profile/"+client);
-      record.whenReady(() => {
-        clientRecord.whenReady(() => {
-          var pendingMeetings = record.get('pendingMeetings');
-          var clientPendingMeetings = clientRecord.get('pendingMeetings');
-          var requestMeetings = record.get('requestMeetings');
-          var clientRequestMeetings = clientRecord.get('requestMeetings');
-          var messages = record.get('messages');
-          var clientMessages = clientRecord.get('messages');
-
-          var i = -1;
-          messages[client].messages.some(function(item, index) {
-              if(item.active) {
-                i = index;
-                return true;
-              }
-              return false;
-            });
-
-          if (i!=-1) {
-            messages[client].messages.splice(i,1,{user: client, message: "Session Ended", special: "EndedSession", active: false});
-            record.set('messages', messages);
-          }
-
-          i = -1;
-          clientMessages[contact].messages.some(function(item, index) {
-              if(item.active) {
-                i = index;
-                return true;
-              }
-              return false;
-            });
-
-          if (i!=-1) {
-            clientMessages[contact].messages.splice(i,1,{user: client, message: "Session Ended", special: "EndedSession", active: false});
-            clientRecord.set('messages', clientMessages);
-          }
-
-          clientPendingMeetings.splice(clientPendingMeetings.indexOf(contact), 1);
-          requestMeetings.splice(requestMeetings.indexOf(client), 1);
-          record.set('pendingMeetings', pendingMeetings);
-          clientRecord.set('pendingMeetings', clientPendingMeetings);
-          clientRecord.set('meeting','');
-          record.set('meeting','');
         });
       });
     }
